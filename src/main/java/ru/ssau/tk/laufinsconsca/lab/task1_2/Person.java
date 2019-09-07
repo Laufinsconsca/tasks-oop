8b2a8f3e0b162176daaa935a3cecd0cc53d213ae
Laufinsconsca <walle.log@yandex.ru>
2019-09-07 19:36:53 +0400

    Complete task 1.2

---
+++ src/main/java/ru/ssau/tk/laufinsconsca/lab/task1_2/Person.java
@@ -0,0 +1,62 @@
+package ru.ssau.tk.laufinsconsca.lab.task1_2;
+
+class Person {
+    private String firstName;
+    private String lastName;
+    private int passportId;
+
+    Person(){}
+
+    Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    Person(int passportId){
+        this.passportId = passportId;
+    }
+
+    Person(String firstName, String lastName, int passportId){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = passportId;
+    }
+
+    String getFirstName() {
+        return firstName;
+    }
+
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    String getLastName() {
+        return lastName;
+    }
+
+    void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    int getPassportId() {
+        return passportId;
+    }
+
+    void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+}
+
+class Task1_2{
+    public static void main(String[] args) {
+        Person firstHuman = new Person();
+        Person secondHuman = new Person("Peter", "Craig");
+        Person thirdHuman = new Person(246429);
+        Person fourthHuman = new Person("Elizabeth", "Bell", 436546);
+
+        System.out.println("First human: unknown");
+        System.out.println("Second human: " + secondHuman.getFirstName() + " " + secondHuman.getLastName() + ", passport ID: unknown");
+        System.out.println("Third human: noname, passport ID: " + thirdHuman.getPassportId());
+        System.out.println("Fourth human: " + fourthHuman.getFirstName() + " " + fourthHuman.getLastName() + ", passport ID: " + fourthHuman.getPassportId());
+    }
+}
