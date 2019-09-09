26d4d0d6c802292602d96ef804ed629791f641ec
Laufinsconsca <walle.log@yandex.ru>
2019-09-09 21:51:33 +0400

    Some changes in accordance with the conditions of the tasks

--- src/main/java/ru/ssau/tk/laufinsconsca/tasks/task1_3/Point.java
+++ src/main/java/ru/ssau/tk/laufinsconsca/tasks/task1_3/Point.java
@@ -25,9 +25,13 @@ class Task1_3{
         Point p1 = new Point(1.5,2,3);
         Point p2 = new Point(4,1,-3.5);
         Point p3 = new Point(3.1,-0.5,2);
+        Point testPoint = new Point(0,0,0);
+
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
-        System.out.println(Points.length(p1));
+
+        Points.sum(p1,p2,p3);
+        System.out.println(p3);
     }
 }
