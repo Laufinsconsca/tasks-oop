ec93544a692e6499d4aed8595fe9f24e52b558ed
Laufinsconsca <walle.log@yandex.ru>
2019-10-28 22:25:16 +0400

    Task 3.2 completed. Latin letters have positive byte code values, cyrillic - negative

--- src/main/java/ru/ssau/tk/laufinsconsca/tasks/strings/Strings.java
+++ src/main/java/ru/ssau/tk/laufinsconsca/tasks/strings/Strings.java
@@ -1,11 +1,18 @@
 package ru.ssau.tk.laufinsconsca.tasks.strings;
 
 public class Strings {
-    public static char[] stringToCharArray(String string) {
+    public static char[] getChars(String string) {
         char[] chars = new char[string.length()];
         for (int i = 0; i < string.length(); i++) {
             chars[i] = string.charAt(i);
         }
         return chars;
+    }
+
+    public static void printBytes(String string) {
+        byte[] bytes = string.getBytes();
+        for (int i = 0; i < string.length(); i++) {
+            System.out.println(bytes[i]);
+        }
     }
 }
--- src/test/java/ru/ssau/tk/laufinsconsca/tasks/strings/StringsTest.java
+++ src/test/java/ru/ssau/tk/laufinsconsca/tasks/strings/StringsTest.java
@@ -2,16 +2,22 @@ package ru.ssau.tk.laufinsconsca.tasks.strings;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class StringsTest {
 
     @Test
     public void testStringToCharArray() {
         String string = "Test string";
-        char[] chars = Strings.stringToCharArray(string);
+        char[] chars = Strings.getChars(string);
         for (int i = 0; i < chars.length; i++) {
             System.out.println(chars[i]);
         }
+    }
+
+    @Test
+    public void testPrintBytes() {
+        String firstString = "Test string";
+        String secondString = "Тестовая строка";
+        Strings.printBytes(firstString);
+        Strings.printBytes(secondString);
     }
 }
